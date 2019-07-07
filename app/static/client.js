72458969e78fdee04c9ac34dd56738b1113b9554
Abhijith Chandran <45108441+cabhijith@users.noreply.github.com>
2019-07-07 21:02:22 +0530

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -30,7 +30,7 @@ function analyze() {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
       el("result-label").innerHTML = `Result = ${response["result"]}`;
-      el("result-label").innerHTML = `Result = ${response["inf"]}`;
+      
     }
     el("analyze-button").innerHTML = "Analyze";
   };
