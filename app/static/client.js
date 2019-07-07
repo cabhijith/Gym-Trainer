e896d716b3a9f46cbd8f2f118fccc5c2e1314d79
Abhijith Chandran <45108441+cabhijith@users.noreply.github.com>
2019-07-07 12:34:29 +0530

    Changing Text

--- app/static/client.js
+++ app/static/client.js
@@ -18,7 +18,7 @@ function analyze() {
   var uploadFiles = el("file-input").files;
   if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
 
-  el("analyze-button").innerHTML = "Analyzing...";
+  el("analyze-button").innerHTML = "Finding out...";
   var xhr = new XMLHttpRequest();
   var loc = window.location;
   xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,
