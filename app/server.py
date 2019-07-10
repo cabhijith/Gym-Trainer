c300fc88673c67c45cf1fae11a8d2dc9ac7ee09e
Abhijith Chandran <45108441+cabhijith@users.noreply.github.com>
2019-07-10 17:12:42 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -63,9 +63,9 @@ async def analyze(request):
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
-    prediction = learn.predict(img)[0]
     result = info[str(prediction)]
-    return JSONResponse({'inf': str(result)})
+    prediction = learn.predict(img)[0]
+    return JSONResponse({'result': str(prediction)})
 #return JSONResponse({'result': str(prediction),
 #                        'inf' : str(info[prediction])})
     
