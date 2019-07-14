dd1ed54eb2d295082f6ef2a04d7c2e1b7628d7a7
Abhijith Chandran <45108441+cabhijith@users.noreply.github.com>
2019-07-14 11:05:32 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -16,9 +16,60 @@ export_file_name = 'New_P100.pkl'
 classes = ['dipstation', 'Battle', 'BenchPress', 'InclineBenchPress', 'HammerStrengthmachine', 'LatPullDownMachine', 'PecDeckMachine', 'PullupBar', 'DumbBells', 'tricepbars', 'PreacherBench', 'HandgripExerciser', 'reversehyper', 'Plyometric', 'airresistance', 'Stair', 'Ankle', 'LegCurlMachine', 'LegPressMachine', 'LegExtensionMachine', 'HackSquatMachine', 'CalfMachines', 'LegAbductionAbductionMachine', 'prowler', 'Mini', 'Inversion', 'Vibration', 'PowerRack', 'MaxiClimber', 'StretchingMachine', 'SmithMachine', 'Suspension', 'CablesandPulleys', 'KettleBells', 'Roman', 'AbdominalBench', 'AbCoaster', 'Stationary', 'CruiserBikes', 'FixieBikes', 'MountainBike', 'RecumbentBikes', 'RoadBikes', 'SpinBikes', 'Comfort', 'Treadmill', 'Mini_Exercise_\ufeffBikes', 'metalplates', 'Medicine', 'Pedometers', 'Pull', 'BloodGlucoseMeter', 'GPSWatches', 'GymnasticsGrips&Gloves', 'hoverboard', 'JumpRope', 'ResistanceBand', 'YogaMat', 'Fitness', 'barbells', 'WallBall', 'FoamRoller', 'Stabilityball', 'AgilityLadder', 'BalanceBoards', 'BalanceBoards']
 path = Path(__file__).parent
 
-title = {"KettleBells" : "Kettle Bells"}
-info = { "KettleBells" : "Kettle Balls One of the most ancient and efficient pieces of strength training equipment is the kettlebell. Consisting of an iron ball with a handle, there are hundreds of exercises which you can do using a kettlebell.It’s important to be careful and precise about posture as these work on multiple core muscles. There are a number of exercises and movements that can be paired with the kettlebell as it is quite diverse on its own. One can easily do a full body workout with just kettlebells and different weight modulation."}
-ques = {"KettleBells" : "What is the use of KettleBells?" }
+Dic_Info = {
+    
+    "dipstation" : "An incredibly effective piece of equipment on which you can you can perform a variety of exercises. It has 2 arms and a large base which increases stability and prevents it from toppling over.A dip bar or station is one of the best options for increased upper body muscularity"
+    "BenchPress" : "A device which can help you get that perfect rock hard chest you’ve always dreamt of. Mainly used in upper body strength training exercises along with a barbell.This is one of those gym cornerstone pieces of equipment that everyone has to go to one time or another. It is vital for building upper body strength and musculature. Find out the right way to bench from the experts."
+    "InclineBenchPress" : "The inclined bench press is a variation of the bench press, using which you can perform strength training exercises at an elevated height. The shoulders and upper chest area can be targeted.Incline bench presses develop your upper chest musculature while also being a bit more joint-friendly than the other variations."
+    "HammerStrengthmachine" : "A plate loaded device which focuses on the body’s natural path of motion. You can fully explore the advantages of converging and diverging arcs of movement.A hammer strength machine lets you lift a lot more than when you lift free weights. You need to remember not to overexert yourself."
+    "LatPullDownMachine" : "The lat pulldown machine is a strength training device with a padded seat, thigh support and a long bar hanging from an upper rod. You can work your lats using this machine.If you are not able to handle pull-ups, this could be a good alternative. You can vary your grips on the pull-down rod to work with different parts of your upper body."
+    "PecDeckMachine" : "The best machine to isolate your pectoral muscles and give them a good workout. You can perform many exercises such as chest flys, butterfly, etc. using a pec deck machine.This machines is particularly beneficial to build chest and shoulder muscles, and also enhance arms strength and stability. The upper body muscles are squeezed together, causing the pectoralis major to expand and contract. This is what builds the muscle and also toughens the tissue fibers. When the exercise on this machine is done with appropriate weights, it does prove to be quite effective."
+    "Treadmill"  : "When you think of cardio workouts, treadmills are the first to come to mind. They are used to help you achieve a walking or running motion while staying in one place. Using a treadmill regularly can help you lose weight and build strength."
+}
+
+title = {
+      "dipstation" : "Dip Station"
+      "BenchPress" : "Bench Press"
+      "InclineBenchPress" : "Incline Bench Press"
+      "HammerStrengthmachine" : "Hammer Strength Machine"
+      "LatPullDownMachine" : "Lat Pull-Down Machine"
+      "PecDeckMachine" : "Pec Deck Machine"
+      "Treadmill": "Treadmill"
+
+
+}
+
+Dic_How_Often = {
+        "dipstation" : "Dips work your chest and arms so you need to ideally supplement this with your arm and chest workouts. You can do this on chest and arm days but just make sure you don’t hit them too hard consecutively."
+        "BenchPress" : "If you are pushing heavy, have rest days in between so your chest can recover."
+         "InclineBenchPress" : "Inclined bench is part of the chest exercise, so the best answer would be when you have to work on your chest."
+         "HammerStrengthmachine" : "It’s a good movement to supplement your biceps/arms workout. Remember to not overdo it and give your arms about 2 days rest before you work the same muscle group."
+         "LatPullDownMachine" : "You can do lat pulldowns twice every week without fatiguing your muscles."
+         "PecDeckMachine" : "Can be used once a week, with varying weights."
+         "Treadmill" : "The frequency will depend on the level of impact of your workouts. Treadmills can be used every day, when opting for low-intensity exercise. For high-intensity runs, it is better to stick to alternate days."
+
+}
+
+Dic_Muscles_Worked = {
+        "dipstation" : "Chest, triceps, front shoulders"
+        "BenchPress" : "Chest, triceps, deltoids, traps and back"
+        "InclineBenchPress" : "Upper chest, triceps, deltoids."
+       "HammerStrengthmachine" : "Lats, chest, middle back, shoulders and triceps"
+        "LatPullDownMachine" : "Lats, deltoids, trapezius and rhomboids"
+        "PecDeckMachine" : "The pectoralis major, pectoralis minor and serratus anterior."
+        "TreadMill" : "Running or walking on a treadmill primarily involves the cardiovascular system and the lower body. The other muscles involved are the hamstrings, quadriceps, calves and glutes."
+}
+
+Dic_Tips = {
+        "dipstation" : "If you want to work your chest during dips just make sure that you lean your body forwards a bit. When you want to work your triceps, you need to stay upright throughout the movement."
+        "BenchPress" : "If you want to build a good chest, remember that form is more important than poundage."
+        "InclineBenchPress" : "Most of the rules of straight bench press apply here like not hyperextending the back or not bouncing it off your chest."
+        "HammerStrengthmachine" : "The action is similar to a shoulder press but a bit more at an angle. The key is to maintain your form and lift smooth rather than jerk it."
+        "LatPullDownMachine" : "When executing this movement, make sure that you don’t hyperextend your back too much."
+        "PecDeckMachine" : "Pair up the pec deck fly along with bench press and bent over cable crossover flys for better results."
+        "Treadmill" : "Always make sure that you warm up before using the treadmill. Start with a slight incline and ensure that it is not too steep. With practice, you can work on improving your stride count.
+"
+}
                          
 app = Starlette()
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
@@ -67,14 +118,20 @@ async def analyze(request):
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
-    resultb = info[str(prediction)]
-    ques_json = ques[str(prediction)]
     title_json = title[str(prediction)]
-    return JSONResponse({'result': str(resultb), 'ques': str(ques_json), 'title' : str(title_json)})
+    info_json = Dic_Info[str(prediction)]
+    often_json = Dic_How_Often[str(prediction)]
+    muscles_json = Dic_Muscles_Worked[str(prediction)]
+    tips_json = Dic_Tips[str(prediction)]
+    
+    
+    
+    
+    
+    return JSONResponse({'title': str(title_json), 'info': str(info_json), 'often' : str(often_json), 'muscles' : str(muscles_json), 'tips' : str(tips_json)})
     
         
-#return JSONResponse({'result': str(prediction),
-#                        'inf' : str(info[prediction])})
+
     
 
 
